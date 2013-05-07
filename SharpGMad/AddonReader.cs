0c8f015277d1901d0e1bc67a0b60ec046b367e66
Whisperity <whisperity@gmail.com>
2013-05-07 09:51:33 +0200

    Split main file into separate assemblies

---
+++ SharpGMad/AddonReader.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Bootil;
+
+namespace Addon
+{
+    class Reader
+    {
+        public Reader()
+        {
+            //Clear();
+        }
+
+        //
+        // Load an addon (call Parse after this succeeds)
+        //
+        /*public bool ReadFromFile(string strName)
+        {
+            
+        }*/
+
+        protected AutoBuffer m_buffer;
+        protected char m_fmtversion;
+        protected string m_name;
+        protected string m_author;
+        protected string m_desc;
+        protected string m_type;
+        protected List<Format.FileEntry> m_index;
+        protected ulong m_fileblock;
+
+        List<string> m_tags;
+    }
+}
---
+++ SharpGMad/AddonWhitelist.cs
@@ -0,0 +1,64 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Addon
+{
+    class Whitelist
+    {
+        private static string[] Wildcard = new string[]{
+            "maps/*.bsp",
+			"maps/*.png",
+			"maps/*.nav",
+			"maps/*.ain",
+			"sound/*.wav",
+			"sound/*.mp3",
+			"lua/*.lua",
+			"materials/*.vmt",
+			"materials/*.vtf",
+			"materials/*.png",
+			"models/*.mdl",
+			"models/*.vtx",
+			"models/*.phy",
+			"models/*.ani",
+			"models/*.vvd",
+			"gamemodes/*.txt",
+			"gamemodes/*.lua",
+			"scenes/*.vcd",
+			"particles/*.pcf",
+			"gamemodes/*/backgrounds/*.jpg",
+			"gamemodes/*/icon24.png",
+			"gamemodes/*/logo.png",
+			"scripts/vehicles/*.txt",
+			"resource/fonts/*.ttf",
+			null
+        };
+
+        //
+        // Call on a filename including relative path to determine
+        // whether file is allowed to be in the addon.
+        //
+        public static bool Check(string strName)
+        {
+            bool bValid = false;
+
+            for (int i = 0; ; i++)
+            {
+                if (bValid || Wildcard[i] == null)
+                {
+                    break;
+                }
+
+                bValid = TestWildcard(Wildcard[i], strName);
+            }
+
+            return bValid;
+        }
+
+        private static bool TestWildcard(string wildcard, string strName)
+        {
+            string pattern = "^" + Regex.Escape(wildcard).Replace(@"\*", ".*").Replace(@"\?", ".") + "$";
+            Regex regex = new Regex(pattern, RegexOptions.IgnoreCase);
+
+            return (regex.IsMatch(strName));
+        }
+    }
+}
